32009fd0e6681f22ca5b34ace80c3e5554199cb8
aca23si <sismail3@sheffield.ac.uk>
2025-06-20 23:36:53 +0100

    Finish basic preprocessing for fakeav and asvspoof datasets - tested on 10 files

--- src/data/preprocess_datasets.py
+++ src/data/preprocess_datasets.py
@@ -1,35 +1,50 @@
 import os, numpy as np, librosa
-from ffmpeg import input as ffmpeg_inp # Avoid namespace ambiguitiy
+from ffmpeg import input as ffmpeg_inp
 
-# Converts raw audio file to 16khz mono wav and mel-spect array
+# Number of files to process for quick testing
+SUBSET = 10
+
+# Converts raw audio file to 16khz mono wav and mel-spectrogram array
 def convert_and_melspectrogram(src_path, out_dir):
-    os.makedirs(out_dir, exist_ok = True)
+    os.makedirs(out_dir, exist_ok  = True)
     
     # Extract base filename without extension
     stem = os.path.splitext(os.path.basename(src_path))[0]
     wav = os.path.join(out_dir, stem + ".wav")
     npy = os.path.join(out_dir, stem + ".npy")
-    
+
     # Convert to 16 kHz mono wav
     ffmpeg_inp(src_path).output(wav, ar = 16000, ac = 1).run(quiet = True)
-    
-    # Loads audio waveform from converted wav
-    y, samp_rate = librosa.load(wav,samp_rate = 16000)
-    
-    # Convert to 64-band mel-spectrogram
-    mels = librosa.feature.melspectrogram(y,samp_rate = samp_rate, n_mels = 64)
-    
-    # Saves mel-spectrogram as binary
+
+    # Load and compute 64-band mel-spectrogram, and save as binary array
+    y, sr = librosa.load(wav, sr = 16000)
+    mels = librosa.feature.melspectrogram(y, sr = sr, n_mels = 64)
     np.save(npy, mels)
 
+    print(f" -- Processed {stem} -- ")
+
 if __name__ == "__main__":
-    # Processes files
-    for subset in ["fakeav", "asvspoof"]:
-        raw = f"data/raw/{subset}"
-        prep = f"data/preprocessed/{subset}"
-        for file in os.listdir(raw):
-            if subset == "fakeav" and file.lower().endswith(".mp4"):
-                convert_and_melspectrogram(os.path.join(raw, file), prep)
-            if subset == "asvspoof" and file.lower().endswith(".wav"):
-                convert_and_melspectrogram(os.path.join(raw, file), prep)
-    print("----Preprocessing complete----")
+    
+    # FakeAVCeleb
+    raw = "data/raw/fakeav"
+    prep = "data/preprocessed/fakeav"
+    files = [
+        fn for fn in os.listdir(raw)
+        if fn.lower().endswith((".mp4", ".wav"))
+    ]
+    files = files[:SUBSET]
+    for fn in files:
+        convert_and_melspectrogram(os.path.join(raw, fn), prep)
+    
+    #ASVspoof2019 LA dev-set
+    raw_asv = "data/raw/asvspoof/LA/ASVspoof2019_LA_dev/flac"
+    prep_asv = "data/preprocessed/asvspoof"
+    files_asv = [
+        fn for fn in os.listdir(raw_asv)
+        if fn.lower().endswith(".flac")
+    ]
+    files_asv = files_asv[:SUBSET]
+    for fn in files_asv:
+        convert_and_melspectrogram(os.path.join(raw_asv, fn), prep_asv)
+    
+    print("--- Preprocessing complete ---")
